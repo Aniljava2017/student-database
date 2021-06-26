f1c70a6533d05aaedf19b3c48fd2213758310d2b
hp <anil.java2017@gmail.com>
2021-06-26 16:38:19 +0530

    Student Database Connection Added Endpoints

--- src/main/java/com/example/studentdatabase/controller/StudentController.java
+++ src/main/java/com/example/studentdatabase/controller/StudentController.java
@@ -13,7 +13,7 @@ import java.util.Optional;
 @RestController
 public class StudentController<optional> {
     @Autowired
-  // List<Student> studentList=new ArrayList<>();
+
     StudentJpaRepository studentJpaRepository;
 
    /* @RequestMapping(value = "/all", method = RequestMethod.GET)
@@ -33,7 +33,7 @@ public class StudentController<optional> {
 
 
     @RequestMapping(value = "/get_std", method = RequestMethod.GET)
-    public List<Student> getAllEmployee() {
+    public List<Student> getAllStudent() {
         return studentJpaRepository.findAll();
     }
 
@@ -41,14 +41,53 @@ public class StudentController<optional> {
     public void removeStudent(@PathVariable Long id){
         studentJpaRepository.deleteById(id);
     }
+
     @RequestMapping(value = "/search_name/{name}",method = RequestMethod.GET)
     public Student findStudentName(@PathVariable String name){
         System.out.println("Name is ----" + name);
         return studentJpaRepository.findByName(name);
     }
+
     @RequestMapping(value = "/search_id/{id}",method = RequestMethod.GET)
     public Optional<Student> findStudentId(@PathVariable Long id){
         return studentJpaRepository.findById(id);
     }
 
+    @RequestMapping(value = "/update_std",method = RequestMethod.PUT)
+    public Student updateStudent(@RequestBody Student student){
+        return studentJpaRepository.save(student);
+    }
+
+    @RequestMapping(value = "/search_by_name/{name}",method = RequestMethod.GET)
+    public Student findbystudentname(@PathVariable String name){
+        System.out.println("Name is ------"+name);
+        String var_name=name;
+        return studentJpaRepository.findByName(var_name);
+    }
+
+    List<Student> studentList=new ArrayList<>();
+    //PathVariable
+    @RequestMapping(value = "/type/{message}")
+    public String m1(@PathVariable String message){
+        return message;
+    }
+    // RequestParam
+    @RequestMapping("/requestparam")
+    public  int demo(@RequestParam int id){
+        return id;
+    }
+    // Added Student
+    @RequestMapping(value = "/addstudent",method = RequestMethod.POST)
+    public void add(@RequestBody Student student){
+        studentList.add(student);
+    }
+    // Get Student
+    @RequestMapping(value = "/getstudent",method = RequestMethod.GET)
+    public List<Student> getStudentList(){
+        return studentList;
+    }
+
+    public void test(){
+        studentJpaRepository.findByName("name");
+    }
 }
